51d8f49aa51b837104753f8566cb87d5f8558864
jvramoss <devjoaoramos@gmail.com>
2023-09-17 23:53:19 -0300

    :tada: routes and pages in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from './styles/global';
 import { Router } from "./Router";
 import {BrowserRouter} from "react-router-dom"
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
