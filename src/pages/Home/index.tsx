3deb8313d1f17bf3ca160abef380d486a67aa2e4
jvramoss <devjoaoramos@gmail.com>
2023-09-28 18:30:47 -0300

    feat styles home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,48 @@
-import {Plus ,Minus  } from "@phosphor-icons/react";
+import {Play } from "@phosphor-icons/react";
+import {HomeContainer,FormContainer,CountDownContainer,Separator,StartCountDownButton,TaskInput,MinutesAmountInput} from "./styles"
 export function Home() {
     return (
-      <div>
-        <form action="">
-        <label htmlFor="text">Vou focar em </label>
-        <input id="text" type="text" placeholder="Dê um nome ao seu projeto"/>
-        <span>durante</span>
-        <input type="number" />
-        <button> <Plus size={24}/> </button>
-  
-        <span>minutos</span>
-        <button> <Minus size={24}/> </button>
-          <button></button>
+      <HomeContainer>
+        <form >
+
+          <FormContainer>
+
+            <label htmlFor="text">Vou focar em </label>
+            <TaskInput id="text" type="text" placeholder="Dê um nome ao seu projeto"/>
+
+
+            <label htmlFor="minutesAmount" >durante</label>
+
+            {/* <button> <Plus size={24}/> </button> */}
+            
+            <MinutesAmountInput type="number" 
+            id="minutesAmount" 
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+
+            />
+            {/* <button> <Minus size={24}/> </button> */}
+
+            <span>minutos.</span>
+          
+          </FormContainer>
+
+          <CountDownContainer>
+            <span>0</span>
+            <span>0</span>
+            <Separator>:</Separator>
+            <span>0</span>
+            <span>0</span>
+          </CountDownContainer>
+
+          <StartCountDownButton>
+            <Play/>
+            Começar
+          </StartCountDownButton>
+
         </form>
-      </div>
+      </HomeContainer>
     )
   }
