534570a237577b20e477af01c89db814c5f60590
jvramoss <devjoaoramos@gmail.com>
2023-10-09 23:54:32 -0300

    fix index

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,6 +21,7 @@ const newCycleFormValidationSchema = zod.object({
     .number()
     .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
